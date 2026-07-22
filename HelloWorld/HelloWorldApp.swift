b6c620ba531f7f3b701d12122889382aaf6e3978
frank <frank.saar@slicelife.com>
2026-07-22 16:02:29 +0100

    initial commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Frank Saar on 22/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by Frank Saar on 22/07/2026.
+//
+
+import Testing
+@testable import HelloWorld
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
